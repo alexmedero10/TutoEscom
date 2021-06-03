f9daf7dbd1058e1d51656837c7d7e340812766e5
alexmedero10 <alexmedero10@gmail.com>
2021-06-03 13:00:26 -0500

    cambioColor

--- TutoEscom/settings.py
+++ TutoEscom/settings.py
@@ -79,10 +79,9 @@ WSGI_APPLICATION = 'TutoEscom.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 # Password validation
